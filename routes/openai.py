c50459c8f417e03c6697a0a8b4d3fd86118e3f52
gxf <270375482@qq.com>
2024-01-15 14:14:58 +0800

    feat: add openai proxy

--- routes/openai.py
+++ routes/openai.py
@@ -13,27 +13,32 @@ router = APIRouter(prefix="/openai")
 @router.get("")
 async def get_openai():
     logger.info("openai begin")
-    client = OpenAI(api_key=Config().openai_api_key)
-
-
-    response = client.chat.completions.create(
-    model="gpt-4-vision-preview",
-    messages=[
-        {
-        "role": "user",
-        "content": [
-            {"type": "text", "text": "这幅图画的是什么"},
-            {
-              "type": "image_url",
-              "image_url": {
-                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+    client = OpenAI(api_key=Config().openai_api_key, base_url="http://openai-proxy-openai-proxy-dejvykwdsv.us-west-1-vpc.fcapp.run")#"https://openai-proxy-openai-proxy-dejvykwdsv.us-west-1.fcapp.run")
+    # client = OpenAI(api_key=Config().openai_api_key, base_url="https://openai-proxy-openai-proxy-dejvykwdsv.us-west-1.fcapp.run")
+# 
+    try:
+      response = client.chat.completions.create(
+      model="gpt-4-vision-preview",
+      messages=[
+          {
+          "role": "user",
+          "content": [
+              {"type": "text", "text": "这幅图画的是什么"},
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
               },
-            },
-        ],
-        }
-    ],
-    max_tokens=300,
-    )
+          ],
+          }
+      ],
+      max_tokens=300,
+      )
+    except Exception as e:
+      logger.error(e)
+      return {"message": "error"}
+      
 
     logger.info(response.choices[0])
     
